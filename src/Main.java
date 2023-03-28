db500408d056ca8d466b1b2f324895601a7c8a15
KennyLVN <kenny2011@live.dk>
2023-03-28 13:54:41 +0200

    altering of table

--- src/Main.java
+++ src/Main.java
@@ -13,15 +13,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-// laptop = catergory id 1, computer = 2, monitor = 3
-
-
-
-
-    //add    data to list computer
-       // ProductService.getInstance().addComputer(myComputer);
-
-
     }
 }
